e388da7ab68ff556532d04c4b1f20cf2bf5d1f48
Yash <yashmittal709.ym@gmail.com>
2022-05-29 04:23:35 +0530

    swag comp

---
+++ src/pages/swag1.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './swag1.css'
+
+function swag1() {
+  return (
+    <div className='swagComponent'>
+      <div className='swagBox1'>
+        <img src='https://swagdecorative.com/static/media/1.5a2209cfb11a6dc7346c.jpeg' alt='Laminates'/>
+      </div>
+      <div className='swagBox2'>
+        <img src='https://swagdecorative.com/static/media/2.e1ba5cdfc96a13aee1ee.jpeg' alt='Veneers'/>
+      </div>
+    </div>
+  )
+}
+
+export default swag1
